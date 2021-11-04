73b4d5840eafa96369749376bd7e442870f66bb3
Padsala Tushal <57517785+padsalatushal@users.noreply.github.com>
2021-11-04 11:20:10 +0530

    add supported version

--- Activator.bat
+++ Activator.bat
@@ -1,1 +1,28 @@
 @echo off
+title Windows 7 / 8 / 8.1 / 10 / 11 Activator By Padsala Tushal
+cls
+echo =====================================
+echo #Copyright: github.com/padsalatushal
+echo =====================================
+echo.
+echo #Supported Products:
+echo - Windows 7 Starter
+echo - Windows 7 Home Basic
+echo - Windows 7 Home Premium
+echo - Windows 7 Professional
+echo - Windows 7 Enterprise
+echo - Windows 7 Ultimate
+echo - Windows 8
+echo - Windows 8 Pro
+echo - Windows 8 Enterprise
+echo - Windows 8.1
+echo - Windows 8.1 Pro
+echo - Windows 8.1 Enterprise
+echo - Windows 10 Home
+echo - Windows 10 Pro
+echo - Windows 10 Enterprise
+echo - Windows 10 Education
+echo - Windows 10 Pro Education
+echo - Windows 10 Enterprise LTSB
+echo - Windows 10 Pro for Workstations
+echo - Windows 11 Home
